36457c84a0e0d7614166c23a3f155b46bbc25c83
aiorbd <ratulhasan3a@gmail.com>
2025-10-04 23:29:26 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,6 +3,10 @@ import fetch from "node-fetch";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("✅ Stream Proxy is Running. Use /proxy endpoint.");
+});
+
 app.get("/proxy", async (req, res) => {
   const targetUrl = "https://laaaaaaaal.dupereasy.com/slh/Y29NR1RBV1JTSGdwQTBvcEdZTUdjL1VxdnYwWUVaNjAxME1zSDFjQVE5aFF2VlFnNWFOc0NTYnpOdHpheUJzZTNMSjc0Rkp1cU12TjhUYWdEVGRFUElFNjNRPT0/master.m3u8";
 
@@ -11,7 +15,7 @@ app.get("/proxy", async (req, res) => {
       headers: {
         "Referer": "https://liveboxpro.com/",
         "Origin": "https://liveboxpro.com",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
       }
     });
 
@@ -28,5 +32,4 @@ app.get("/proxy", async (req, res) => {
   }
 });
 
-// For Vercel: export as default
 export default app;
